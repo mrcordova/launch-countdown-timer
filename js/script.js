9aa02145e81a4d969c99bc09f0c3510cf95f7fec
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-07-14 18:53:40 -0700

    logic for count down

---
+++ js/script.js
@@ -0,0 +1,40 @@
+({
+  plugins: ["jsdom-quokka-plugin"],
+  jsdom: { file: "index.html" }, // Located in project root
+});
+
+const countdownDate = new Date();
+countdownDate.setTime(countdownDate.getTime() + 14 * 24 * 60 * 60 * 1000);
+
+const days = document.querySelectorAll(".days");
+const hours = document.querySelectorAll(".hours");
+const minutes = document.querySelectorAll(".minutes");
+const seconds = document.querySelectorAll(".seconds");
+
+window.addEventListener("load", () => {
+  //   startCountdown();
+  setInterval(function () {
+    const now = new Date().getTime();
+
+    const distance = countdownDate.getTime() - now;
+    const daysNum = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hoursNum = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsNum = Math.floor((distance % (1000 * 60)) / 1000);
+
+    for (const day of days) {
+      day.textContent = `${daysNum}`;
+    }
+    for (const hour of hours) {
+      hour.textContent = `${hoursNum}`;
+    }
+    for (const minute of minutes) {
+      minute.textContent = `${mins}`;
+    }
+    for (const second of seconds) {
+      second.textContent = `${secondsNum}`;
+    }
+  }, 1000);
+});
